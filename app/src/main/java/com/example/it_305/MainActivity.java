1f1af896d85dd40a64f9a02eb2214c9ca488fc83
Android <omaryasser3004@gmail.com>
2022-03-31 21:40:59 +0200

    haha

---
+++ app/src/main/java/com/example/it_305/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.it_305;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    Uri image_uri;
+    private static final int PERMISSION_REQUEST = 112;
+    private static final int RESULT_LOAD_IMAGE = 123;
+    public static final int IMAGE_CAPTURE_CODE = 654;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView2);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        {
+            if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+            {
+                String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE ,Manifest.permission.WRITE_EXTERNAL_STORAGE };
+                requestPermissions(permissions,PERMISSION_REQUEST);
+
+            }
+        }
+
+        imageView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                {
+                    if(checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
+                            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED)
+                    {
+                        String[] permissions = {Manifest.permission.CAMERA ,Manifest.permission.WRITE_EXTERNAL_STORAGE };
+                        requestPermissions(permissions,PERMISSION_REQUEST);
+
+                    }
+                    else
+                        openCamera();
+                }
+                return false;
+            }
+
+        });
+        //choose from galleery
+        imageView.setOnClickListener(view -> {
+            Intent galleryIntent = new Intent(Intent.ACTION_PICK ,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(galleryIntent , RESULT_LOAD_IMAGE);
+
+        });
+
+    }
+
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the Camera");
+
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT , image_uri);
+        startActivityForResult(cameraIntent , IMAGE_CAPTURE_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == IMAGE_CAPTURE_CODE && resultCode == RESULT_OK)
+        {
+            Bitmap bitmap = UriToBitmap(image_uri);
+            imageView.setImageBitmap(bitmap);
+        }
+        else if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null)
+        {
+            image_uri = data.getData();
+            Bitmap bitmap = UriToBitmap(image_uri);
+            imageView.setImageBitmap(bitmap);
+        }
+    }
+
+    private Bitmap UriToBitmap(Uri selectedFileUri)
+    {
+        try {
+            ParcelFileDescriptor parcelFileDescriptor = getContentResolver().openFileDescriptor(selectedFileUri , "r");
+            FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+            Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+            parcelFileDescriptor.close();
+            return image ;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return  null;
+    }
+}
